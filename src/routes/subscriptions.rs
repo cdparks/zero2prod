314d803a49165b483e66aa0fe9a88f8d35a628c4
Chris Parks <christopher.daniel.parks@gmail.com>
2021-12-07 14:12:27 -0800

    Update POST /subscriptions to actually insert data

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
@@ -7,6 +10,23 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let query = sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, created_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    );
+
+    match query.execute(pool.as_ref()).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("{}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
