e86b89843dbf6b8a44f21c82fb301a2420c4320d
Chris Parks <christopher.daniel.parks@gmail.com>
2021-12-07 21:21:25 -0800

    Add layered settings based on environment

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::settings;
 use zero2prod::startup;
@@ -10,12 +10,13 @@ async fn main() -> std::io::Result<()> {
     telemetry::init(subscriber);
 
     let settings = settings::load().expect("failed to read config file");
-    let pool = PgPool::connect(&settings.database.url())
+    let pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&settings.database.url())
         .await
         .expect("failed to connect to postgres");
 
-    let address = format!("127.0.0.1:{}", settings.port);
-    let listener = TcpListener::bind(address)?;
+    let listener = TcpListener::bind(settings.app.address())?;
 
     startup::run(listener, pool)?.await
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,6 +1,6 @@
 #[derive(serde::Deserialize)]
 pub struct Settings {
-    pub port: u16,
+    pub app: App,
     pub database: Database,
 }
 
@@ -29,8 +29,63 @@ impl Database {
     }
 }
 
+#[derive(serde::Deserialize)]
+pub struct App {
+    pub port: u16,
+    pub host: String,
+}
+
+impl App {
+    pub fn address(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
+}
+
 pub fn load() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
-    settings.merge(config::File::with_name("config"))?;
+    let path = std::env::current_dir().expect("failed to determine current directory");
+    let dir = path.join("settings");
+
+    // Read base config
+    settings.merge(config::File::from(dir.join("base")).required(true))?;
+
+    // Determine environment, defaulting to Dev
+    let env: Env = std::env::var("APP_ENV")
+        .unwrap_or_else(|_| "dev".into())
+        .try_into()
+        .expect("failed to parse APP_ENV");
+
+    // Read env-specific config
+    settings.merge(config::File::from(dir.join(env.as_str())).required(true))?;
+
+    // Read environment variables prefixed with `APP_`
+    settings.merge(config::Environment::with_prefix("APP").separator("_"))?;
+
     settings.try_into()
+}
+
+pub enum Env {
+    Dev,
+    Prod,
+}
+
+impl Env {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Env::Dev => "dev",
+            Env::Prod => "prod",
+        }
+    }
+}
+
+impl TryFrom<String> for Env {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "dev" => Ok(Self::Dev),
+            "prod" => Ok(Self::Prod),
+            other => Err(format!("Unrecognized environment {}; use dev or prod", other)),
+        }
+    }
 }
